f59ede531ef38cd1e07a537af8f318314c029857
akashmirror11 <105482805+akashmirror11@users.noreply.github.com>
2022-05-29 23:39:05 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,11 @@
 import asyncio
+from time import time
 from os import environ
-from pyrogram import Client, filters, idle
+from pyrogram import Client, filters
 
 API_ID = int(environ.get("API_ID"))
 API_HASH = environ.get("API_HASH")
 BOT_TOKEN = environ.get("BOT_TOKEN")
-SESSION = environ.get("SESSION")
 TIME = int(environ.get("TIME"))
 GROUPS = []
 for grp in environ.get("GROUPS").split():
@@ -16,13 +16,6 @@ for usr in environ.get("ADMINS").split():
 
 START_MSG = "<b>Hai {},\nI'm a simple bot to delete group messages after a specific time</b>"
 
-
-User = Client(name="user-account",
-              session_string=SESSION,
-              api_id=API_ID,
-              api_hash=API_HASH,
-              workers=300
-              )
 
 
 Bot = Client(name="auto-delete",
@@ -37,8 +30,8 @@ Bot = Client(name="auto-delete",
 async def start(bot, message):
     await message.reply(START_MSG.format(message.from_user.mention))
 
-@User.on_message(filters.chat(GROUPS))
-async def delete(user, message):
+@bot.on_message(filters.chat(GROUPS))
+async def delete(bot, message):
     try:
        if message.from_user.id in ADMINS:
           return
@@ -48,14 +41,6 @@ async def delete(user, message):
     except Exception as e:
        print(e)
        
-User.start()
-print("User Started!")
 Bot.start()
 print("Bot Started!")
 
-idle()
-
-User.stop()
-print("User Stopped!")
-Bot.stop()
-print("Bot Stopped!")
